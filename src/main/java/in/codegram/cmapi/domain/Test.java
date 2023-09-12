92c74995d4d23ad2c07327cfa7d32163f98cb2d8
piyushturey17 <119047161+piyushturey17@users.noreply.github.com>
2023-09-12 08:54:30 +0530

    Update Test.java
    changes from git to sts

--- src/main/java/in/codegram/cmapi/domain/Test.java
+++ src/main/java/in/codegram/cmapi/domain/Test.java
@@ -17,6 +17,7 @@ public class Test {
 	
 	//This is to check from sts to git
 	//This is to check from git to sts
+	//This is to check changes from git to sts
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
